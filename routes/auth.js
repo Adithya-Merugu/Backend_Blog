c8152595b4a06a53203848dc72a7b6c5eff91135
Adithya <adithya2003porps@gmail.com>
2023-09-07 09:53:42 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,6 @@ const bcrypt = require("bcrypt");
 
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
